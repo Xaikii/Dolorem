dbbccce3019e24edd7164d9144e91c6d4350c9a6
Xaikii <xaikii@outlook.de>
2021-11-06 18:26:31 +0100

    initial

---
+++ elijah/scripts/helper/logi.lua
@@ -0,0 +1,47 @@
+function ternary(cond, T, F)
+    if cond then return T else return F end
+end
+
+function angleComp(alpha, beta)
+    local d = math.abs(alpha - beta)
+    return math.min(360 - d, d)
+end
+
+function directionToVectorAngle(directionValue, player)
+    --if directionValue == 0 then
+    --    return 180
+    --elseif directionValue == 1 then
+    --    return -90
+    --elseif directionValue == 2 then
+    --    return 0
+    --elseif directionValue == 3 then
+    --    return 90 
+    --elseif directionValue == -1 and player:GetLastDirection() ~= nil then
+    --    return player:GetLastDirection():GetAngleDegrees()
+    --end
+    if directionValue ~= -1 then
+        return 180 - (((math.min(directionValue, 1) * 4) - directionValue) * 90)
+    end
+    return player:GetLastDirection():GetAngleDegrees()
+end
+
+function vectorAngleConvert(angle)
+    if angle % 360 > 180 then
+        return angle - 360
+    end
+    return angle
+end
+
+function maskToNumber(mask, base)
+    return math.log(mask+1, base)
+end
+
+function highestIndexTable(table) --Number+1, start at 1 if something found, else it's 0
+    local n = -1
+    for index, _ in ipairs(table) do
+        if n < index then
+            n = index
+        end
+    end
+    return ternary(n>-1, n+1, 0)
+end
---
+++ elijah/scripts/items/passives/cursed_bones.lua
@@ -0,0 +1,88 @@
+require("scripts.helper.roomHelper")
+require("scripts.helper.functionHelper")
+
+local cursedBones = Isaac.GetItemIdByName("Cursed Bones")
+
+local r = 0
+local cachedBoneHearts = 200
+
+function CursedBonesPostPlayer(_, player)
+    local numCursedBones = player:GetCollectibleNum(cursedBones)
+
+    if numCursedBones >=1 then
+        if cachedBoneHearts > player:GetBoneHearts() and cachedBoneHearts ~= 200 then
+            cachedBoneHearts = player:GetBoneHearts()
+            SpawnPickup(
+                PickupVariant.PICKUP_HEART, 
+                HeartSubType.HEART_BLACK, 
+                true, 
+                Vector(0,0), 
+                player,
+                Game():GetRoom()
+            )
+        else 
+            cachedBoneHearts = player:GetBoneHearts()
+        end
+    end
+
+    if r<numCursedBones then
+        CursedBonesRemoveSpikesGrid()
+
+        r = r + 1
+    end
+end
+
+function CursedBonesRemoveHealthOnFloor(_)
+    for i=0, Game():GetNumPlayers() do
+        local player = Isaac.GetPlayer(i)
+
+        if (player:GetMaxHearts()/2) > 1 and player:HasCollectible(cursedBones) then
+            player:AddMaxHearts(-1, true)
+        end
+        --r = 0
+    end
+end
+
+function CursedBonesRemoveSpikesGrid(_)
+    local room = Game():GetRoom()
+    local roomShape = room:GetRoomShape()
+
+    for i=0, Game():GetNumPlayers() do
+        local player = Isaac.GetPlayer(i)
+
+        if player:HasCollectible(cursedBones) then
+            for i=0, GetPossibleGrindIndiciesFromRoom(roomShape) do
+        
+                local gridE = room:GetGridEntity(i)
+        
+                if gridE ~= nil then
+                    if gridE:GetType() == GridEntityType.GRID_ROCK_SPIKED then
+                        gridE.State = 4
+                    elseif gridE:ToDoor() ~= nil then
+                        if gridE:ToDoor().TargetRoomType == RoomType.ROOM_CURSE or 
+                        gridE:ToDoor().CurrentRoomType == RoomType.ROOM_CURSE then
+                            --Isaac.DebugString(tostring(gridE:GetSprite():GetAnimation()))
+                            gridE.VarData = 1
+                            gridE.GetSprite():GetFilename()
+                            --gridE:GetSprite():ReplaceSpritesheet(0, "door_04_selfsacrificeroomdoor.png")
+                        end
+                    elseif gridE:ToSpikes() ~= nil then
+                        if gridE:GetType() == GridEntityType.GRID_SPIKES and
+                        room:GetType() ~= RoomType.ROOM_SACRIFICE then
+                            gridE.State = 1
+                            gridE:GetSprite():Play("Unsummon", true)
+                        elseif gridE:GetType() == GridEntityType.GRID_SPIKES_ONOFF then
+                            gridE.State = 1
+                            gridE:ToSpikes().Timeout = 2147483646
+                            gridE:GetSprite():Play("Unsummon", true)
+                        end 
+                    end
+                end
+            end
+        end
+    end
+end
+
+DoloremMod:AddCallback(ModCallbacks.MC_POST_PLAYER_UPDATE, CursedBonesPostPlayer)
+DoloremMod:AddCallback(ModCallbacks.MC_POST_NEW_LEVEL, CursedBonesRemoveHealthOnFloor)
+DoloremMod:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, CursedBonesRemoveSpikesGrid)
