105aeb8c0dfe23e57f932dbf6edc659228e39d74
Xaikii <xaikii@outlook.de>
2021-12-07 09:53:49 +0100

    Delete elijah directory

--- elijah/main.lua
+++
@@ -1,6 +0,0 @@
-DoloremMod = RegisterMod("Dolorem", 1)
-
-require('scripts.items.passives.ectoplasm')
-require('scripts.items.passives.stare')
-require('scripts.items.passives.demonic_resolve')
-require('scripts.items.passives.cursed_bones')
